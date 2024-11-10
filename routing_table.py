bf3c750918b6b69ee73449e469696a5f049a19af
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-11-10 17:53:35 -0300

    TF - Fix Serialize Routing Table

--- routing_table.py
+++ routing_table.py
@@ -44,8 +44,8 @@ class RoutingTable:
         return sorted(ips)
 
     def serialize_routing_table_to_string(self) -> str:
-        return "@".join([f"{route[0]}-{route[1]}" for route in self.routes])
-    
+        return "".join([f"@{route[0]}-{route[1]}" for route in self.routes])
+
     def parse_string_to_routing_table(self, table_string: str):
         table_rows = re.split(r'@', table_string)
         table: list[TableRow] = []
