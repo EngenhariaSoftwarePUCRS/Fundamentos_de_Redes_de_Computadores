65e2415f61c00d5556d704f620a338537b06657b
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-11-10 19:30:38 -0300

    TF - Fix Updated Metric
    Co-authored-by: Carolina Ferreira <carolmic@users.noreply.github.com>
    Co-authored-by: Nicholasspoltidesouza <Nicholasspoltidesouza@users.noreply.github.com>

--- router.py
+++ router.py
@@ -122,11 +122,13 @@ def handle_route(message: str, sender: Address):
             metric = int(metric) + 1
             routing_table.register_route(ip, metric, sender_ip)
             should_resend = True
-        elif int(metric) < route_to_ip[1]:
-            # If I already know how to get to this IP, update the metric if it is lower
-            metric = int(metric) + 1
-            routing_table.update_route(ip, metric, sender_ip)
-            should_resend = True
+        else:
+            old_metric = route_to_ip[1]
+            new_metric = int(metric) + 1
+            if new_metric < old_metric:
+                # If I already know how to get to this IP, update the metric if it is lower
+                routing_table.update_route(ip, new_metric, sender_ip)
+                should_resend = True
 
     # Remove routes that are no longer received
     known_ips = routing_table.get_ips_from_routes()
